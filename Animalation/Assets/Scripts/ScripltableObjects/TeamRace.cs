97458a5aa89f4a7ca8b443a54118141f09d82af5
Konstantin Kaiser <konstantin.kaiser95@gmail.com>
2018-02-27 18:07:33 +0100

    Added ScriptableObjects (TeamRace, CharacterClass)

---
+++ Animalation/Assets/Scripts/ScripltableObjects/TeamRace.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Race", menuName = "Race")]
+public class TeamRace : ScriptableObject {
+
+    //public Sprite raceIcon;
+    public string raceName;
+    //public String raceDescription;
+}
